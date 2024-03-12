7f4d307795a64136b04281c3c17c5ede7ab97acf
Helena Jovic <h.jovix@gmail.com>
2024-03-12 12:19:55 +0100

    init project

--- main.go
+++ main.go
@@ -9,22 +9,39 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func initDB() *gorm.DB {
-	connectionStr := "root:root@tcp(localhost:3306)/students?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(connectionStr), &gorm.Config{})
-	if err != nil {
-		print(err)
-		return nil
-	}
+    connectionStr := "postgres://postgres:super@localhost:5432/explorer-v1?sslmode=disable"
 
-	database.AutoMigrate(&model.Student{})
-	database.Exec("INSERT IGNORE INTO students VALUES ('aec7e123-233d-4a09-a289-75308ea5b7e6', 'Marko Markovic', 'Graficki dizajn')")
-	return database
+    database, err := gorm.Open(postgres.Open(connectionStr), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+        return nil
+    }
+
+    if err := database.Exec("CREATE SCHEMA IF NOT EXISTS your_schema").Error; err != nil {
+        log.Fatalf("Failed to create schema: %v", err)
+        return nil
+    }
+
+    if err := database.Table("your_schema.students").AutoMigrate(&model.Student{}); err != nil {
+        log.Fatalf("Failed to auto migrate: %v", err)
+        return nil
+    }
+
+    if err := database.Exec("INSERT INTO your_schema.students (id, name, major) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING",
+        "aec7e123-233d-4a09-a289-75308ea5b7e6", "Marko Markovic", "Graficki dizajn").Error; err != nil {
+        log.Fatalf("Failed to insert record: %v", err)
+        return nil
+    }
+
+    return database
 }
+
+
 
 func startServer(handler *handler.StudentHandler) {
 	router := mux.NewRouter().StrictSlash(true)
